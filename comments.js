c18e392478521212e7f2ae99cef0303028cb6c2f
Juan Sebastian <96669725+devJuanS@users.noreply.github.com>
2023-11-06 01:21:50 +0000

    Copilot third-b commit

--- comments.js
+++ comments.js
@@ -1,2 +1,17 @@
 // create web server
-// here suggested code by Copilot
+// Import the required modules
+const http = require('http');
+
+// Create the server
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  // Send a response to the client
+  res.end('Hello, World!');
+});
+
+// Start the server
+server.listen(3000, 'localhost', () => {
+  console.log('Server running at http://localhost:3000/');
+});
